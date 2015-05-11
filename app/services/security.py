290f768e3d7523bf4884b868d0e8e0b8591c4d3e
Bartosz Kowalik <bartekviper@gmail.com>
2015-05-11 22:38:15 +0200

    Security

--- app/services/security.py
+++ app/services/security.py
@@ -28,3 +28,29 @@ class Security:
     def add_person_to_group(db, person, group):
         sec = Relationship(group, Security.IS_MEMBER_OF, person)
         db.create(sec)
+
+    @staticmethod
+    def __accessable(db, user, resource, permissions):
+        relations = db.match(user, Security.SECURITY, resource)
+        for rel in relations:
+            if rel[Security.SECURITY].contains(permissions):
+                return True
+        return False
+
+    @staticmethod
+    def has_access(db, user, resource):
+        return Security.__accessable(db, user, resource, Permissions.R)
+
+    @staticmethod
+    def can_write(db, user, resource):
+        return Security.__accessable(db, user, resource, Permissions.W)
+
+
+class Permissions:
+
+    RW = "RW"
+    R = "R"
+    W = "W"
+
+    def __init__(self):
+        pass
